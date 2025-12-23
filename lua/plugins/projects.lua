267c6bb12480d9f8af8ce93042b22a05105f7834
Ryan <ryanalvaro.emmyson@gmail.com>
2025-12-23 11:11:01 +0700

    Update projects.lua

--- lua/plugins/projects.lua
+++ lua/plugins/projects.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		require("telescope").load_extension("projects")
 		require("project_nvim").setup({
-			patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "README", "LICENSE" },
+			patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "README", "LICENSE", "ThisIsAProject" },
 		})
 	end,
 }
