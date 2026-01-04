6846afa42c2a818d7832e63a8eaededbed7cd97f
Ryanswa28h <ryanalvaro.emmyson@gmail.com>
2026-01-04 11:56:00 +0700

    Changed keybinds for consistency

--- lua/plugins/jupyter.lua
+++ lua/plugins/jupyter.lua
@@ -67,9 +67,9 @@ return {
 				end
 			end
 
-			vim.keymap.set("n", "<leader>nbin", init_notebook, { desc = "Initialize Notebook Header" })
-			vim.keymap.set("n", "<leader>in", "<cmd>MoltenInit<CR>", { desc = "Run Smart Cell" })
-			vim.keymap.set("n", "<leader>mmd", "<cmd>MoltenDelete<CR>", { desc = "Molten Delete" })
+			vim.keymap.set("n", "<leader>rbin", init_notebook, { desc = "Initialize Notebook Header" })
+			vim.keymap.set("n", "<leader>rrn", "<cmd>MoltenInit<CR>", { desc = "Run Smart Cell" })
+			vim.keymap.set("n", "<leader>rmd", "<cmd>MoltenDelete<CR>", { desc = "Molten Delete" })
 			vim.keymap.set("n", "<leader>rr", run_smart_cell, { desc = "Run Smart Cell" })
 			vim.keymap.set("n", "<leader>ra", function()
 				local line_count = vim.api.nvim_buf_line_count(0)
