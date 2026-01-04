8bcb0811c59242a72063a675b3d681c21065816c
Ryanswa28h <ryanalvaro.emmyson@gmail.com>
2026-01-04 11:50:31 +0700

    Changed keybind to activate flash (s -> L & S -> M)

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -6,7 +6,7 @@ return {
 		opts = {},
 		keys = {
 			{
-				"s",
+				"L",
 				mode = { "n", "x", "o" },
 				function()
 					require("flash").jump()
@@ -14,7 +14,7 @@ return {
 				desc = "Flash",
 			},
 			{
-				"S",
+				"M",
 				mode = { "n", "x", "o" },
 				function()
 					require("flash").treesitter()
