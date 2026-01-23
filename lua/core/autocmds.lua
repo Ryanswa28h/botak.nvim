e591dd7e70a448cc0dc189285d6afd1b141bd9aa
Ryanswa28h <ryanalvaro.emmyson@gmail.com>
2026-01-23 09:01:58 +0700

    Added QoL autocmds

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -21,13 +21,66 @@ vim.api.nvim_create_autocmd("User", {
 	end,
 })
 
--- Highlight yanking
+-- highlight yank
 vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+	pattern = "*",
+	desc = "highlight selection on yank",
 	callback = function()
-		vim.highlight.on_yank({
-			higroup = "IncSearch",
-			timeout = 120,
-		})
+		vim.highlight.on_yank({ timeout = 120, visual = true })
+	end,
+})
+
+-- restore cursor to file position in previous editing session
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function(args)
+		local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
+		local line_count = vim.api.nvim_buf_line_count(args.buf)
+		if mark[1] > 0 and mark[1] <= line_count then
+			vim.api.nvim_win_set_cursor(0, mark)
+			-- defer centering slightly so it's applied after render
+			vim.schedule(function()
+				vim.cmd("normal! zz")
+			end)
+		end
+	end,
+})
+
+-- auto resize splits when the terminal's window is resized
+vim.api.nvim_create_autocmd("VimResized", {
+	command = "wincmd =",
+})
+
+-- no auto continue comments on new line
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("no_auto_comment", {}),
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+	end,
+})
+
+-- syntax highlighting for dotenv files
+vim.api.nvim_create_autocmd("BufRead", {
+	group = vim.api.nvim_create_augroup("dotenv_ft", { clear = true }),
+	pattern = { ".env", ".env.*" },
+	callback = function()
+		vim.bo.filetype = "dosini"
+	end,
+})
+
+-- show cursorline only in active window enable
+vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
+	group = vim.api.nvim_create_augroup("active_cursorline", { clear = true }),
+	callback = function()
+		vim.opt_local.cursorline = true
+	end,
+})
+
+-- show cursorline only in active window disable
+vim.api.nvim_create_autocmd({ "WinLeave", "BufLeave" }, {
+	group = "active_cursorline",
+	callback = function()
+		vim.opt_local.cursorline = false
 	end,
 })
 
@@ -35,7 +88,7 @@ local neotree = false
 vim.api.nvim_create_autocmd("VimEnter", {
 	once = true,
 	callback = function(data)
-		vim.cmd("ShowkeysToggle") -- Enable showkeys on startup
+		-- vim.cmd("ShowkeysToggle") -- Enable showkeys on startup
 		if vim.fn.isdirectory(data.file) == 1 then
 			if neotree then
 				vim.cmd("cd " .. data.file)
