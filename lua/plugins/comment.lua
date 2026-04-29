fb5a51040fbd977c6f18aff27ec129424e49b3a7
Ryanswa28h <12345678+Ryanswa28h@users.noreply.github.com>
2026-04-29 16:20:15 +0700

    Made comment.nvim the default option for keymap `gcc` and now falls back to #

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,29 +1,21 @@
--- Easily comment visual regions/lines
 return {
 	"numToStr/Comment.nvim",
-	opts = {},
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
 	config = function()
-	    local opts = { noremap = true, silent = true }
-		vim.keymap.set("n", "<C-_>", require("Comment.api").toggle.linewise.current, opts)
-		vim.keymap.set("n", "<C-c>", require("Comment.api").toggle.linewise.current, opts)
-		vim.keymap.set("n", "<C-/>", require("Comment.api").toggle.linewise.current, opts)
-		vim.keymap.set(
-			"v",
-			"<C-_>",
-			"<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
-			opts
-		)
-		vim.keymap.set(
-			"v",
-			"<C-c>",
-			"<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
-			opts
-		)
-		vim.keymap.set(
-			"v",
-			"<C-/>",
-			"<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
-			opts
-		)
+		local ts_pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
+
+		require("Comment").setup({
+			pre_hook = function(ctx)
+				local cs = ts_pre_hook(ctx)
+
+				if cs == nil or cs == "" or not cs:find("%%s") then
+					return "# %s"
+				end
+
+				return cs
+			end,
+		})
 	end,
 }
