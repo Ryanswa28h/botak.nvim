66bd56f92ecd339360859a5b5d36e767c2cd8f8d
Ryanswa28h <12345678+Ryanswa28h@users.noreply.github.com>
2026-05-01 16:41:59 +0700

    Added comments and cleaned up code

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 require("core.autocmds")
 require("core.options")
 require("core.keymaps")
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -9,6 +10,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 		error("Error cloning lazy.nvim:\n" .. out)
 	end
 end
+
 ---@type vim.Option
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
@@ -29,6 +31,7 @@ vim.schedule(function()
 		},
 	})
 end)
+
 vim.api.nvim_set_hl(0, "SnippetTabstop", { bg = "NONE", fg = "NONE" })
 
 require("lazy").setup({
@@ -46,26 +49,26 @@ require("lazy").setup({
 	require("plugins.projects"),
 	require("plugins.dashboard"),
 	require("plugins.indent-blankline"),
-	-- require("plugins.harpoon"),
+	-- require("plugins.harpoon"), -- NOTE: Requires specific keybinds that may be taken by the OS/DE/WM
 	require("plugins.undotree"),
 	require("plugins.gitsigns"),
 	require("plugins.comment"),
 	require("plugins.diffview"),
 	require("plugins.jupyter"),
-	-- require("plugins.codeium"),
-	-- require("plugins.copilot"),
+	-- require("plugins.codeium"), -- INFO: Enable if you don't want to use copilot
+	-- require("plugins.copilot"), -- INFO: Replaced by blink-cmp copilot integration
 	require("plugins.noice"),
 	require("plugins.tmux"),
 	-- require("plugins.comfy-num-lines"), -- Makes number columns use left hand numbers only.  NOTE: Breaks line diagnostics respecting gitsigns
 	require("plugins.navigation"),
 	require("plugins.diagnostics"),
-	-- require("plugins.avante"),
-	-- require("plugins.opencode"),
+	-- require("plugins.avante"), -- NOTE: Requires a custom Avante server to be running so disabled by default
+	-- require("plugins.opencode"), -- NOTE: Requires a custom OpenCode server to be running, and is not yet fully functional, so disabled by default
 	require("plugins.debug"),
 	require("plugins.rainbowdelimiters"),
 	require("plugins.prelive"),
 	require("plugins.nvzone"),
-	-- require("plugins.image"),
+	-- require("plugins.image"), -- NOTE: Disabled by default because it requires a image loading terminal backend
 	require("plugins.fold"),
 	require("plugins.mason"),
 	require("plugins.lsp"),
