8abd4e3fdb7fb0910f0de25cb94ee1f568e5130b
Ryanswa28h <ryanalvaro.emmyson@gmail.com>
2026-05-02 15:15:54 +0700

    Added keybinds to accept suggestions using Ctrl+1-9 and changed priorities

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -37,11 +37,75 @@ return {
 			["<C-j>"] = { "select_next", "fallback" },
 			["<C-k>"] = { "select_prev", "fallback" },
 			-- ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-n>"] = { "show", "show_documentation", "hide_documentation" },
 			-- ["<CR>"] = {},
 			-- ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
 			-- ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
 			-- ["<C-l>"] = { "snippet_forward", "fallback" },
 			-- ["<C-h>"] = { "snippet_backward", "fallback" },
+			["<A-1>"] = {
+				function(cmp)
+					if cmp.is_menu_visible() then
+						return cmp.accept({ index = 1 })
+					end
+				end,
+			},
+			["<A-2>"] = {
+				function(cmp)
+					if cmp.is_menu_visible() then
+						return cmp.accept({ index = 2 })
+					end
+				end,
+			},
+			["<A-3>"] = {
+				function(cmp)
+					if cmp.is_menu_visible() then
+						return cmp.accept({ index = 3 })
+					end
+				end,
+			},
+			["<A-4>"] = {
+				function(cmp)
+					if cmp.is_menu_visible() then
+						return cmp.accept({ index = 4 })
+					end
+				end,
+			},
+			["<A-5>"] = {
+				function(cmp)
+					if cmp.is_menu_visible() then
+						return cmp.accept({ index = 5 })
+					end
+				end,
+			},
+			["<A-6>"] = {
+				function(cmp)
+					if cmp.is_menu_visible() then
+						return cmp.accept({ index = 6 })
+					end
+				end,
+			},
+			["<A-7>"] = {
+				function(cmp)
+					if cmp.is_menu_visible() then
+						return cmp.accept({ index = 7 })
+					end
+				end,
+			},
+			["<A-8>"] = {
+				function(cmp)
+					if cmp.is_menu_visible() then
+						return cmp.accept({ index = 8 })
+					end
+				end,
+			},
+			["<A-9>"] = {
+				function(cmp)
+					if cmp.is_menu_visible() then
+						return cmp.accept({ index = 9 })
+					end
+				end,
+			},
 		},
 
 		appearance = {
@@ -79,19 +143,30 @@ return {
 
 		sources = {
 			default = {
-				"copilot", -- comment here to disable copilot suggestions
-				"snippets",
 				"lsp",
+				"snippets",
+				"copilot", -- comment here to disable copilot suggestions
 				"path",
 				"buffer",
 			},
 			providers = {
+				lsp = {
+					name = "LSP",
+					module = "blink.cmp.sources.lsp",
+					score_offset = 100, -- Give LSP the highest priority
+				},
+				snippets = {
+					name = "Snippets",
+					module = "blink.cmp.sources.snippets",
+					score_offset = 80, -- Snippets second
+				},
 				copilot = {
 					name = "Copilot",
 					module = "blink-cmp-copilot",
-					score_offset = 100,
+					score_offset = 50, -- AI can be fuzzy so we can put it below snippets and lsp
 					async = true,
 				},
+				-- Paths and buffers last
 			},
 		},
 
@@ -160,8 +235,72 @@ return {
 				["<C-j>"] = { "select_next", "fallback" },
 				["<C-k>"] = { "select_prev", "fallback" },
 				["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+				["<C-n>"] = { "show", "show_documentation", "hide_documentation" },
 				-- ["<Tab>"] = { "select_next", "fallback" },
 				-- ["<S-Tab>"] = { "select_prev", "fallback" },
+				["<A-1>"] = {
+					function(cmp)
+						if cmp.is_menu_visible() then
+							return cmp.accept({ index = 1 })
+						end
+					end,
+				},
+				["<A-2>"] = {
+					function(cmp)
+						if cmp.is_menu_visible() then
+							return cmp.accept({ index = 2 })
+						end
+					end,
+				},
+				["<A-3>"] = {
+					function(cmp)
+						if cmp.is_menu_visible() then
+							return cmp.accept({ index = 3 })
+						end
+					end,
+				},
+				["<A-4>"] = {
+					function(cmp)
+						if cmp.is_menu_visible() then
+							return cmp.accept({ index = 4 })
+						end
+					end,
+				},
+				["<A-5>"] = {
+					function(cmp)
+						if cmp.is_menu_visible() then
+							return cmp.accept({ index = 5 })
+						end
+					end,
+				},
+				["<A-6>"] = {
+					function(cmp)
+						if cmp.is_menu_visible() then
+							return cmp.accept({ index = 6 })
+						end
+					end,
+				},
+				["<A-7>"] = {
+					function(cmp)
+						if cmp.is_menu_visible() then
+							return cmp.accept({ index = 7 })
+						end
+					end,
+				},
+				["<A-8>"] = {
+					function(cmp)
+						if cmp.is_menu_visible() then
+							return cmp.accept({ index = 8 })
+						end
+					end,
+				},
+				["<A-9>"] = {
+					function(cmp)
+						if cmp.is_menu_visible() then
+							return cmp.accept({ index = 9 })
+						end
+					end,
+				},
 			},
 			completion = {
 				menu = {
