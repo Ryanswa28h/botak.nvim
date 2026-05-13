8cdf9924cc7a818b2a46dd234990cecae6e5e3b5
Ryanswa28h <ryanalvaro.emmyson@gmail.com>
2026-05-13 19:45:53 +0700

    Changed theme manageement

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,7 +1,6 @@
--- Do <leader>sth to set a new theme
 local theme_cache = vim.fn.stdpath("data") .. "/last_theme.lua"
 
-local function save_theme_state(theme, transparent)
+local function save_state(theme, transparent)
 	local file = io.open(theme_cache, "w")
 	if file then
 		file:write(string.format("return { theme = '%s', transparent = %s }", theme, tostring(transparent)))
@@ -9,109 +8,81 @@ local function save_theme_state(theme, transparent)
 	end
 end
 
-local function load_theme_state()
+local function load_state()
 	local f = loadfile(theme_cache)
-	if f then
-		return f()
-	end
-	return { theme = "tokyonight-night", transparent = false }
+	return f and f() or { theme = "onedark", transparent = false }
 end
 
-local saved_state = load_theme_state()
-local bg_transparent = saved_state.transparent
-local current_theme = saved_state.theme
+local state = load_state()
 
-local apply_transparency = function(state)
-	bg_transparent = state
-	local theme = vim.g.colors_name or current_theme
-
-	if theme:find("catppuccin") then
-		require("catppuccin").setup({ transparent_background = bg_transparent })
-	elseif theme:find("tokyonight") then
-		require("tokyonight").setup({ transparent = bg_transparent })
-	elseif theme:find("nord") then
-		vim.g.nord_disable_background = bg_transparent
-	end
-
-	vim.cmd.colorscheme(theme)
-	save_theme_state(theme, bg_transparent)
+local function apply_theme_config(theme_name, transparent)
+	local status, _ = pcall(function()
+		if theme_name:find("onedark") then
+			require("onedark").setup({ style = "darker", transparent = transparent })
+		elseif theme_name:find("catppuccin") then
+			require("catppuccin").setup({ transparent_background = transparent })
+		elseif theme_name:find("tokyonight") then
+			require("tokyonight").setup({ transparent = transparent })
+		elseif theme_name:find("nord") then
+			vim.g.nord_disable_background = transparent
+		end
+	end)
+	return status
 end
 
-local enable_bg = function()
-	apply_transparency(false)
-end
-local disable_bg = function()
-	apply_transparency(true)
-end
-
+-- Persistence Autocmd
 vim.api.nvim_create_autocmd("ColorScheme", {
 	callback = function()
-		-- Prevent saving during the "preview" phase of Telescope
 		if vim.v.event.abort then
 			return
 		end
-
-		local new_theme = vim.g.colors_name
-		save_theme_state(new_theme, bg_transparent)
+		save_state(vim.g.colors_name, state.transparent)
 	end,
 })
 
+-- Global Toggle Keymap
+vim.keymap.set("n", "<leader>bg", function()
+	state.transparent = not state.transparent
+	apply_theme_config(vim.g.colors_name, state.transparent)
+	vim.cmd.colorscheme(vim.g.colors_name)
+	save_state(vim.g.colors_name, state.transparent)
+end, { desc = "Toggle Transparency" })
+
+-- Final application logic run after Lazy loads the specs
+local function finalize_theme()
+	apply_theme_config(state.theme, state.transparent)
+	-- pcall prevents Neovim from crashing if the 'state.theme' plugin is disabled
+	pcall(vim.cmd.colorscheme, state.theme)
+end
+
 return {
+	{
+		"navarasu/onedark.nvim",
+		lazy = false,
+		priority = 1000,
+		enabled = true, -- Toggle this
+		config = finalize_theme,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		enabled = true, -- Toggle this
+		config = finalize_theme,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 1000,
+		enabled = false, -- Toggle this
+		config = finalize_theme,
+	},
 	{
 		"shaunsingh/nord.nvim",
 		lazy = false,
 		priority = 1000,
-		config = function()
-			vim.g.nord_contrast = true
-			vim.g.nord_borders = true
-			vim.g.nord_disable_background = bg_transparent
-		end,
-	},
-
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		opts = {
-			transparent_background = bg_transparent,
-			integrations = {
-				notify = true,
-				mini = { enabled = true, indentscope_color = "sky" },
-			},
-		},
-	},
-
-	{
-		"folke/tokyonight.nvim",
-		priority = 1000,
-		lazy = false,
-		opts = {
-			style = "night",
-			transparent = bg_transparent,
-			styles = { sidebars = "transparent", floats = "transparent" },
-		},
-		config = function(_, opts)
-			require("tokyonight").setup(opts)
-
-			vim.cmd.colorscheme(current_theme)
-
-			-- vim.keymap.set("n", "<leader>bg", enable_bg, { desc = "UI: Enable Transparency" })
-			-- vim.keymap.set("n", "<leader>dbg", disable_bg, { desc = "UI: Disable Transparency" })
-			-- vim.keymap.set("n", "<leader>sth", function()
-			-- 	Snacks.picker.colorschemes({ layout = "default" })
-			-- end, { desc = "UI: Theme Selector" })
-
-			vim.api.nvim_create_autocmd("ColorScheme", {
-				callback = function()
-					if vim.v.event.abort then
-						return
-					end
-					local new_theme = vim.g.colors_name
-					if new_theme then
-						save_theme_state(new_theme, bg_transparent)
-					end
-				end,
-			})
-		end,
+		enabled = false, -- Toggle this
+		config = finalize_theme,
 	},
 }
