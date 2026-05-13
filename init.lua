95951395b2cbf1a125567ddad50048d0ad7daf28
Ryanswa28h <ryanalvaro.emmyson@gmail.com>
2026-05-13 19:38:19 +0700

    Supermaven

--- init.lua
+++ init.lua
@@ -35,46 +35,63 @@ end)
 vim.api.nvim_set_hl(0, "SnippetTabstop", { bg = "NONE", fg = "NONE" })
 
 require("lazy").setup({
-	require("plugins.mininvim"),
-	require("plugins.snacks"),
-	require("plugins.colortheme"),
-	require("plugins.bufferline"), -- NOTE: Disable if you're advanced enough with global marks, harpoon, telescope or other means of navigation
-	require("plugins.persistence"),
-	require("plugins.lualine"),
-	-- require("plugins.telescope"), -- NOTE: Replaced by Snacks.picker
-	require("plugins.misc"),
-	require("plugins.neotree"),
-	require("plugins.treesitter"),
-	require("plugins.blink"),
-	-- require("plugins.projects"),
-	require("plugins.dashboard"),
-	require("plugins.indent-blankline"),
-	require("plugins.harpoon"), -- NOTE: Requires specific keybinds that may be taken by the OS/DE/WM
-	require("plugins.undotree"),
-	require("plugins.gitsigns"),
-	require("plugins.comment"),
-	require("plugins.diffview"),
-	-- require("plugins.jupyter"), -- INFO: Enable if you want to use Neovim as a Jupyter notebook editor
-	-- require("plugins.codeium"), -- INFO: You could enable if you don't want to use copilot
-	require("plugins.copilot"), -- Ghost text autocompletion
-	require("plugins.noice"), -- Neovim UI overhaul
-	require("plugins.tmux"), -- NOTE: Requires tmux (Disable if you don't use tmux or don't want the integration)
-	-- require("plugins.comfy-num-lines"), -- Makes number columns use left hand numbers only.  NOTE: Breaks line diagnostics respecting gitsigns
-	require("plugins.navigation"),
-	require("plugins.diagnostics"),
-	require("plugins.avante"), -- NOTE: Requires a custom Avante server to be running so disabled by default
-	-- require("plugins.opencode"), -- NOTE: Requires a custom OpenCode server to be running, and is not yet fully functional, so disabled by default
-	require("plugins.debug"),
-	require("plugins.rainbowdelimiters"), -- Rainbow parentheses, functions, statements and brackets
-	require("plugins.prelive"),
-	require("plugins.nvzone"),
-	-- require("plugins.image"), -- NOTE: Disabled by default because it requires a image loading terminal backend
-	-- require("plugins.fold"),
-	-- require("plugins.oil"), -- Oil is a fullscreen file explorer where you edit your files as in a regular neovim buffer
-	require("plugins.mason"),
-	require("plugins.lsp"),
-	require("plugins.linters"),
-	require("plugins.neogit"),
-	require("plugins.conform"),
-	require("plugins.coderunner"),
+	spec = {
+		{ import = "plugins.mininvim" },
+		{ import = "plugins.snacks" },
+		{ import = "plugins.colortheme" },
+		{ import = "plugins.bufferline" }, -- NOTE: Disable if you're advanced enough with global marks, harpoon, telescope or other means of navigation
+		{ import = "plugins.persistence" },
+		{ import = "plugins.lualine" },
+		-- { import = "plugins.telescope" }, -- NOTE: Replaced by Snacks.picker
+		{ import = "plugins.misc" },
+		{ import = "plugins.grug-far" },
+		-- { import = "plugins.neotree" },
+		{ import = "plugins.treesitter" },
+		{ import = "plugins.blink" },
+		-- { import = "plugins.projects" },
+		{ import = "plugins.dashboard" },
+		{ import = "plugins.aerial" },
+		{ import = "plugins.indent-blankline" },
+		{ import = "plugins.harpoon" }, -- NOTE: Requires specific keybinds that may be taken by the OS/DE/WM
+		{ import = "plugins.undotree" },
+		{ import = "plugins.gitsigns" },
+		{ import = "plugins.splits" },
+		{ import = "plugins.comment" },
+		{ import = "plugins.diffview" },
+		-- { import = "plugins.jupyter" }, -- INFO: Enable if you want to use Neovim as a Jupyter notebook editor
+		-- { import = "plugins.codeium" }, -- INFO: You could enable if you don't want to use copilot
+		-- { import = "plugins.copilot" }, -- Ghost text autocompletion
+		{ import = "plugins.supermaven" },
+		{ import = "plugins.noice" }, -- Neovim UI overhaul
+		-- { import = "plugins.comfy-num-lines" }, -- Makes number columns use left hand numbers only.  NOTE: Breaks line diagnostics respecting gitsigns
+		{ import = "plugins.navigation" },
+		{ import = "plugins.diagnostics" },
+		{ import = "plugins.avante" }, -- NOTE: Requires a custom Avante server to be running so disabled by default
+		-- { import = "plugins.opencode" }, -- NOTE: Requires a custom OpenCode server to be running, and is not yet fully functional, so disabled by default
+		{ import = "plugins.debug" },
+		{ import = "plugins.rainbowdelimiters" }, -- Rainbow parentheses, functions, statements and brackets
+		{ import = "plugins.prelive" },
+		{ import = "plugins.nvzone" },
+		-- { import = "plugins.image" }, -- NOTE: Disabled by default because it requires a image loading terminal backend
+		{ import = "plugins.fold" },
+		{ import = "plugins.coderunner" },
+		-- { import = "plugins.oil" }, -- Oil is a fullscreen file explorer where you edit your files as in a regular neovim buffer
+		{ import = "plugins.mason" },
+		{ import = "plugins.lsp" },
+		{ import = "plugins.linters" },
+		{ import = "plugins.neogit" },
+		{ import = "plugins.conform" },
+	},
+	performance = {
+		cache = { enabled = true },
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"netrwPlugin",
+				"tarPlugin",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 })
