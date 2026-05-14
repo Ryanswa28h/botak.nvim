81a374e613a2092a7dd2a74a09d8ec0ac9f813ff
Ryanswa28h <ryanalvaro.emmyson@gmail.com>
2026-05-14 20:26:45 +0700

    Changd `config` to `init` for mini.icons so it can hijack nvim-web-devicons

--- lua/plugins/editor/mini.lua
+++ lua/plugins/editor/mini.lua
@@ -16,7 +16,7 @@ return {
 		"nvim-mini/mini.icons",
 		lazy = true,
 		opts = {},
-		config = function(_, opts)
+		init = function(_, opts)
 			require("mini.icons").setup(opts)
 			MiniIcons.mock_nvim_web_devicons()
 		end,
