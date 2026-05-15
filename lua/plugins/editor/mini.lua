71ff1ecf3290ce13fed838452e6aa874359aeec0
Ryanswa28h <ryanalvaro.emmyson@gmail.com>
2026-05-15 19:35:20 +0700

    Changed it from multiple standalone plugins to the single mini.nvim library

--- lua/plugins/editor/mini.lua
+++ lua/plugins/editor/mini.lua
@@ -1,49 +1,19 @@
 local open_mini_files = function()
 	local buf_name = vim.api.nvim_buf_get_name(0)
 	local path = vim.fs.dirname(buf_name)
-
-	-- If path is nil/empty or it's a special buffer, use home directory
 	if path == nil or path == "." or path == "" then
 		path = vim.uv.os_homedir()
 	end
-
 	require("mini.files").open(path)
 end
 
 return {
-	-- 1. Mini Icons (Load early for UI)
 	{
-		"nvim-mini/mini.icons",
-		lazy = true,
-		opts = {},
-		init = function(_, opts)
-			require("mini.icons").setup(opts)
-			MiniIcons.mock_nvim_web_devicons()
-		end,
-	},
-
-	-- 2. Mini Ai & Surround (Load on text objects/editing)
-	{ "nvim-mini/mini.ai", event = "VeryLazy", opts = {} },
-	{ "nvim-mini/mini.surround", event = "VeryLazy", opts = {} },
-
-	-- 3. Mini Move, Pairs, Operators
-	{ "nvim-mini/mini.move", event = "VeryLazy", opts = {} },
-	{ "nvim-mini/mini.operators", event = "VeryLazy", opts = {} },
-	{ "nvim-mini/mini.pairs", event = "InsertEnter", opts = {} },
-
-	-- 4. Mini Bracketed
-	{ "nvim-mini/mini.bracketed", event = "VeryLazy", opts = {} },
-
-	-- 5. Mini Files (Load only on keypress)
-	{
-		"nvim-mini/mini.files",
+		"nvim-mini/mini.nvim",
 		lazy = false,
+		priority = 1000,
 		keys = {
-			{
-				"-",
-				open_mini_files,
-				desc = "Open MiniFiles at Current Buffer",
-			},
+			{ "-", open_mini_files, desc = "Open MiniFiles at Current Buffer" },
 			{
 				"<leader>E",
 				function()
@@ -52,54 +22,43 @@ return {
 				desc = "Open MiniFiles",
 			},
 		},
-		opts = {
-			windows = { preview = true, width_preview = 65 },
-			options = { use_as_default_explorer = true },
-		},
-	},
-
-	-- 6. Mini Git
-	{
-		"nvim-mini/mini-git",
-		event = "VeryLazy",
-		version = false,
-		opts = {
-			job = { timeout = 5000 },
-		},
-		config = function(_, opts)
-			require("mini.git").setup(opts)
-		end,
-		keys = {
-			{ "<leader>g!", ":Git ", desc = "Git Command" },
-			{ "<leader>gh", "<cmd>lua MiniGit.show_at_cursor()<cr>", desc = "Git History (Cursor)" },
-		},
-	},
-
-	-- 7. Mini Hipatterns
-	{
-		"nvim-mini/mini.hipatterns",
-		event = "VeryLazy",
 		config = function()
-			local hipatterns = require("mini.hipatterns")
+			require("mini.icons").setup()
+			MiniIcons.mock_nvim_web_devicons()
 
-			hipatterns.setup({
-				highlighters = {
-					-- 1. Standard Hex Colors (#aabbcc)
-					hex_color = hipatterns.gen_highlighter.hex_color({ priority = 2000 }),
-
-					-- 2. Shorthand Hex Colors (#abc)
-					shorthand = {
-						pattern = "()#%x%x%x()%f[^%x%w]",
-						group = function(_, _, data)
-							local match = data.full_match
-							local r, g, b = match:sub(2, 2), match:sub(3, 3), match:sub(4, 4)
-							local hex_color = "#" .. r .. r .. g .. g .. b .. b
-							return hipatterns.compute_hex_color_group(hex_color, "bg")
-						end,
-						extmark_opts = { priority = 2000 },
-					},
-				},
+			require("mini.files").setup({
+				windows = { preview = true, width_preview = 65 },
+				options = { use_as_default_explorer = true },
 			})
+
+			-- Equivalent to VeryLazy
+			vim.schedule(function()
+				require("mini.ai").setup({})
+				require("mini.surround").setup({})
+				require("mini.move").setup({})
+				require("mini.operators").setup({})
+				require("mini.bracketed").setup({})
+				require("mini.pairs").setup({})
+				require("mini.git").setup({ job = { timeout = 5000 } })
+				-- require("mini.statusline").setup({})
+
+				local hipatterns = require("mini.hipatterns")
+				hipatterns.setup({
+					highlighters = {
+						hex_color = hipatterns.gen_highlighter.hex_color({ priority = 2000 }),
+						shorthand = {
+							pattern = "()#%x%x%x()%f[^%x%w]",
+							group = function(_, _, data)
+								local match = data.full_match
+								local r, g, b = match:sub(2, 2), match:sub(3, 3), match:sub(4, 4)
+								local hex_color = "#" .. r .. r .. g .. g .. b .. b
+								return hipatterns.compute_hex_color_group(hex_color, "bg")
+							end,
+							extmark_opts = { priority = 2000 },
+						},
+					},
+				})
+			end)
 		end,
 	},
 }
