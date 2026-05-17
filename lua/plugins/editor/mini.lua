3668a56b646f84a20aa82d548e0cfb2f589c7b17
Ryanswa28h <ryanalvaro.emmyson@gmail.com>
2026-05-17 16:10:04 +0700

    Allowed mini.indentscope to co-exist with snacks indent

--- lua/plugins/editor/mini.lua
+++ lua/plugins/editor/mini.lua
@@ -75,6 +75,9 @@ return {
 				require("mini.bracketed").setup({})
 				require("mini.pairs").setup({})
 				require("mini.git").setup({ job = { timeout = 5000 } })
+				require("mini.indentscope").setup({
+					symbol = "",
+				})
 				-- require("mini.statusline").setup(statusline_opts)
 
 				local misc = require("mini.misc")
