c7a6158204539ec67501415180eb828a6272829e
Ryanswa28h <ryanalvaro.emmyson@gmail.com>
2026-05-24 13:44:02 +0700

    Lazyloaded on filetype

--- lua/plugins/coding/autotag.lua
+++ lua/plugins/coding/autotag.lua
@@ -3,5 +3,16 @@ return {
 	-- autoclose tags
 	"windwp/nvim-ts-autotag",
 	event = "InsertEnter",
+	ft = {
+		"html",
+		"xml",
+		"javascriptreact",
+		"typescriptreact",
+		"svelte",
+		"vue",
+		"astro",
+		"php",
+		"markdown",
+	},
 	config = true,
 }
