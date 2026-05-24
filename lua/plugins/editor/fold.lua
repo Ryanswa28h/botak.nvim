2c48c71ae5e9582e360e98bac872ebc3cc6d694f
Ryanswa28h <ryanalvaro.emmyson@gmail.com>
2026-05-24 17:20:56 +0700

    Folds!!

--- lua/plugins/editor/fold.lua
+++ lua/plugins/editor/fold.lua
@@ -4,29 +4,68 @@ return {
 		event = "VeryLazy",
 		dependencies = "kevinhwang91/promise-async",
 		config = function()
-			-- UFO needs a large foldlevel to start with everything open
 			vim.o.foldlevel = 99
 			vim.o.foldlevelstart = 99
 			vim.o.foldenable = true
 
 			require("ufo").setup({
-				close_fold_kinds_for_ft = {
-					default = {},
-				},
+				fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+					local newVirtText = {}
+					local suffix = ""
+					if (endLnum - lnum) > 1 then
+						suffix = (" [%d lines]"):format(endLnum - lnum)
+					else
+						suffix = (" [%d line]"):format(endLnum - lnum)
+					end
+					local sufWidth = vim.fn.strdisplaywidth(suffix)
+					local targetWidth = width - sufWidth
+					local curWidth = 0
+
+					for _, chunk in ipairs(virtText) do
+						local chunkText = chunk[1]
+						local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+
+						if targetWidth > curWidth + chunkWidth then
+							table.insert(newVirtText, chunk)
+						else
+							chunkText = truncate(chunkText, targetWidth - curWidth)
+							table.insert(newVirtText, { chunkText, chunk[2] })
+							chunkWidth = vim.fn.strdisplaywidth(chunkText)
+
+							if curWidth + chunkWidth < targetWidth then
+								suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+							end
+
+							break
+						end
+
+						curWidth = curWidth + chunkWidth
+					end
+
+					table.insert(newVirtText, { suffix, "MoreMsg" })
+
+					return newVirtText
+				end,
 				provider_selector = function(bufnr, filetype, buftype)
+					if filetype == "markdown" then
+						return { "treesitter", "indent" }
+					end
+
 					return { "lsp", "indent" }
 				end,
 			})
 		end,
 	},
-	-- {
-	-- 	"chrisgrieser/nvim-origami",
-	-- 	version = "v1.9",
-	-- 	event = "BufReadPost",
-	-- 	opts = {
-	-- 		pauseFoldsOnSearch = true,
-	-- 	},
-	-- },
+	{
+		"chrisgrieser/nvim-origami",
+		version = "v1.9",
+		event = "BufReadPost",
+
+		opts = {
+			pauseFoldsOnSearch = true,
+			pauseFoldsOnInsert = true,
+		},
+	},
 	{
 		"jghauser/fold-cycle.nvim",
 		event = "VeryLazy",
