79034232b02dc0ecc5bae4492a47f006138e07d6
Ryanswa28h <ryanalvaro.emmyson@gmail.com>
2026-05-27 21:00:19 +0700

    "default" as the default config preset

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-vim.g.configpreset = "minimal" -- "default" or "minimal"
+vim.g.configpreset = "default" -- "default" or "minimal"
 require("config.autocmds")
 require("config.options")
 require("config.keymaps")
